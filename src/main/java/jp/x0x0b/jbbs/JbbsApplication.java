0a663d3468da40b176f958fc326fd5fb8e6a15da
x0x0b <42596409+x0x0b@users.noreply.github.com>
2022-02-26 09:15:41 +0900

    first commit

---
+++ src/main/java/jp/x0x0b/jbbs/JbbsApplication.java
@@ -0,0 +1,13 @@
+package jp.x0x0b.jbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/x0x0b/jbbs/JbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.x0x0b.jbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
